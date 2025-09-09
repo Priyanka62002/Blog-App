70d6f393bda0c0345d9f1d0774c55ad3c546c4ef
Priyanka <pbanerjeepriyanka@gmail.com>
2025-09-10 03:57:55 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout';
+import Input from "./Input";
+import Select from "./Select";
+
+
+
+export {
+
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    PostForm,
+    Login,
+    AuthLayout,
+    Button,
+    PostCard,
+    SignUp,
+    Input,
+    Select
+
+}
